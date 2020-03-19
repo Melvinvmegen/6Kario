c46a8a68b6b4af2640ff26934cb0a052916ffb05
Melvin <melvin.vmegen@gmail.com>
2020-03-19 15:02:24 +0100

    video banner

--- src/Containers/Banner/Banner.js
+++ src/Containers/Banner/Banner.js
@@ -4,7 +4,7 @@ import classes from './Banner.module.css'
 class Banner extends Component {
   render() {
 
-    const videoBanner = "https://vod-progressive.akamaized.net/exp=1584625038~acl=%2A%2F1661935281.mp4%2A~hmac=e73a7bd6ca9bba524d6f7668fd6f4a10c442442c2ea66ba377cbec554479de3b/vimeo-prod-skyfire-std-us/01/2299/15/386498776/1661935281.mp4"
+    const videoBanner = "https://vod-progressive.akamaized.net/exp=1584640898~acl=%2A%2F1661935281.mp4%2A~hmac=d52bac67798d9d5ae4fdb9c56941d68c3e13a579c981951ad57a1e6fc1e0ed58/vimeo-prod-skyfire-std-us/01/2299/15/386498776/1661935281.mp4"
 
     return (
       <div id="section-banner" className={classes.SectionBanner}>
